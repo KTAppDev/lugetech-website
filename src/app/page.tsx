214c274202278d4827f1dfd5a8b79f2e2d2aec5c
Clint Eastman <clint4life@gmail.com>
2023-08-06 19:06:15 -0400

    Spaced the words Luge Tech

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p className="text-4xl font-bold text-center">
-        LugeTech
+        Luge Tech
       </p>
     </main>
   )
