d059914a12470dbe58e4e02316043ede3ed9261e
Clint Eastman <clint4life@gmail.com>
2023-10-02 19:34:51 -0400

    testing divs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <main className=' h-full w-full flex flex-col'>
-      <div id='logo' className='flex flex-row items-center justify-around font-bold text-xl'>
+    <main className=" h-full w-full flex flex-col">
+      <div
+        id="logo"
+        className="flex flex-row items-center justify-around font-bold text-xl"
+      >
         <p> LugeTech </p>
       </div>
-      <div id='profiles' className=' flex flex-row'>
-          <div id='profile-iamge' className='flex flex-col'>
-          <Image 
-          className='flex flex-col rounded-full' 
-          src='https://avatars.githubusercontent.com/u/61248118?v=4' alt='Clint' width={60} height={60} />
+      <div id="profiles" className=" flex flex-col h-full w-full bg-yellow-200">
+        <div
+          id="profile"
+          className="flex flex-row h-1/2 w-full bg-lime-200 gap-2"
+        >
+          <div>
+            <div id="profile-info" className=" flex flex-col w-5/6 gap-2">
+              <div id="info" className=" flex flex-row w-full">
+                info Div
+              </div>
+              <div id="github-info" className=" flex flex-row">
+                Github Div
+              </div>
+            </div>
           </div>
-        <div id='profile'>
-          <div id='profile-name'>Clint</div>
+            <div id="badges" className=" flex flex-1 flex-col">
+              Ba
+            </div>
         </div>
+        <div className=" h-1/2">Projects</div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default page;
