9940b71237e7ac19fdbe7d2bb42a5a936728f173
Clint Eastman <clint4life@gmail.com>
2023-10-03 15:50:41 -0400

    updated divs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,17 +15,31 @@ const page = () => {
           id="profile"
           className="flex flex-row h-1/2 w-full bg-lime-200 gap-2"
         >
-            <div id="profile-info" className=" flex flex-col w-5/6 gap-2">
-              <div id="info" className=" flex flex-row w-full">
-                info Div
+            <div id="profile-info" className=" flex flex-col w-5/6 gap-2 justify-between">
+              <div id="info" className=" flex flex-row w-4/6">
+              <div id="profile-image" className=" flex flex-col w-1/6">
+                <Image className=" rounded-full"
+                  src="https://avatars.githubusercontent.com/u/61248118?v=4"
+                  alt="Profile Picture"
+                  width={70}
+                  height={70}
+                />
+              </div>
+                <div id="profile-name" className=" flex flex-col">
+                  Clint Eastman
+                  <div id="profile-title">
+                    Software Engineer
+                  </div>
+                  <div id="About">I am a software engineer with a passion for building things.</div>
+                  </div>
+                   {/* <div></div> empty div for info that can beb added later */}
               </div>
               <div id="github-info" className=" flex flex-row">
                 Github Div
               </div>
             </div>
-
           <div id="badges" className=" flex flex-col min-w-1/6">
-            Ba
+            BA
           </div>
         </div>
         <div className=" h-1/2">Projects</div>
