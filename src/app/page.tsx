117bc39c39c7460f0ffc4fbb56f26e85ac79397f
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-10-09 19:43:52 -0400

    ui update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,13 +12,13 @@ const page = () => {
         <p> LugeTech</p>
       </div>
       <div id="profiles" className=" flex flex-col md:flex-row h-full w-full ">
-        <div id="profile" className="flex flex-row  w-full  mr-4">
-          <div id="profile-info" className=" flex flex-col w-full gap-4 justify-between ">
+        <div id="profile" className="flex flex-row  w-full h-1/2  mr-4">
+          <div id="profile-info" className=" flex flex-col w-full h-full gap-4 justify-between ">
             <DeveloperInfo name="ktappdev" />
           </div>
         </div>
-        <div id="profile" className="flex flex-row w-full gap-4 ">
-          <div id="profile-info" className=" flex flex-col w-full gap-4 justify-between ">
+        <div id="profile" className="flex flex-row w-full gap-4 h-1/2">
+          <div id="profile-info" className=" flex flex-col w-full h-full gap-4 justify-between ">
             <DeveloperInfo name="clinteastman01" />
           </div>
         </div>
