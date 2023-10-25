26549b524c3a10c6d5fcb85ec3fa6bea27a8d43a
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-10-25 14:34:08 -0400

    updated get-pinned-repos

--- src/app/lib/serverActions.ts
+++ src/app/lib/serverActions.ts
@@ -1,6 +1,6 @@
 'use server'
 import { profile, Repo } from "./interfaces"
-const ghPinnedRepos = require('@kentaylorappdev/get-pinned-repos')
+import { getPinnedRepos } from '@kentaylorappdev/get-pinned-repos'
 
 export const GetGithubProfile = async (username: string): Promise<profile> => {
   return fetch(`https://api.github.com/users/${username}`)
@@ -16,10 +16,10 @@ export const GetGithubRepos = async (username: string): Promise<Repo[]> => {
 
 
 export const GetGithubPinnedRepos = async (username: string): Promise<Repo[]> => {
-  const pinned = await ghPinnedRepos(username)
+  const pinned = await getPinnedRepos(username) as unknown as String[]
   const allrepos = await GetGithubRepos(username)
-  
-  return pinned.map((pinnedRepo:String) => {
-    return allrepos.find((repo:Repo) => repo.full_name === pinnedRepo)
+
+  return pinned.map((pinnedRepo) => {
+    return allrepos.find((repo: Repo) => repo.full_name === pinnedRepo)!
   })
 }
